2afeeb3752be144feb762ce125c74b5c567e09d1
wangxiaobo775 <670713146@qq.com>
2025-10-13 17:34:56 +0800

    fix: 修复 RequestHistoryView 的 Vue 属性顺序 ESLint 错误

--- web/admin-spa/src/views/RequestHistoryView.vue
+++ web/admin-spa/src/views/RequestHistoryView.vue
@@ -35,8 +35,8 @@
           </label>
           <input
             v-model="filters.date"
-            type="date"
             class="w-full rounded-lg border border-gray-300 bg-white px-4 py-2 text-sm text-gray-900 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-100 dark:focus:border-blue-400 dark:focus:ring-blue-800"
+            type="date"
             @change="loadHistory"
           />
         </div>
